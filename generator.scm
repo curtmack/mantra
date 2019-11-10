054820d543fd366d561cb932d28a39c7c3ac0fc6
Curtis Mackie <curtmackevo@gmail.com>
2019-11-10 17:15:52 -0600

    generator.scm: Reduce usage of random bytes where feasible
    Previously we always generated a u64 for each phrase. This is a tad much.
    Now we calculate the number of bytes needed to have a <10% chance of
    resampling. This reduces the amount of kernel entropy consumed with
    --blocking.

--- generator.scm
+++ generator.scm
@@ -32,34 +32,61 @@
 ;; portability.
 (define blocking (make-parameter #f))
 
-;; Generate a bytevector of cryptographically secure random bytes.
+;; Generate a bytevector of `count' cryptographically secure random bytes.
 (define (random-bytes count)
   (call-with-input-file (if (blocking) "/dev/random" "/dev/urandom")
     (lambda (port)
       (get-bytevector-n port count))
     #:binary #t))
 
-;; Generate a random 64-bit unsigned integer.
-(define (random-u64)
-  (let ((bv (random-bytes 8)))
-    (bytevector-u64-native-ref bv 0)))
+;; Generate a random integer with `count' bytes.
+(define (random-uint count)
+  (let ((bv (random-bytes count)))
+    (bytevector-uint-ref bv 0 (native-endianness) count)))
 
-;; The smallest number larger than every 64-bit unsigned integer.
-(define max-u64 (expt 2 64))
+;; The smallest number larger than every unsigned integer of `count' bytes.
+(define (max-uint count)
+  (expt 256 count))
 
-;; Generate a random number in the range of
-(define (uniform-random num)
+;; Compute the size of the bias region when a random number between 0 and
+;; `possibilities' minus 1 is used to select from a vector of size `count'.
+(define (bias-region possibilities count)
+  (mod possibilities count))
+
+;; Compute the resample probability of `bytes' random bytes are used to select
+;; from a vector of size `count'.
+(define (resample-probability bytes count)
+  (let ((possibilities (expt 256 bytes)))
+    ;; If there are fewer possibilities than the vector count, we have already
+    ;; failed. Return a resample rate of 100% to indicate this.
+    (if (< possibilities count)
+        1
+        ;; Otherwise, the resample probability is the size of the bias region
+        ;; divided by the total number of possibilities.
+        (/ (bias-region possibilities count)
+           possibilities))))
+
+;; Compute the optimal number of bytes for a random integer for selecting from
+;; a vector of size count.
+(define (optimal-random-bytes count)
+  ;; The minimum number of bytes such that the probability of a resample is less
+  ;; than 1/10.
+  (let ((desired-resample-rate 1/10))
+    (do ((bytes 1 (1+ bytes)))
+        ((< (resample-probability bytes count) desired-resample-rate)
+         bytes))))
+
+;; Generate a random number in the range from 0 to num.
+(define* (uniform-random num #:optional (bytes (optimal-random-bytes num)))
   (cond
-   ((> num (expt 2 64))
-    (error "Too large for u64" num))
    ;; This is a shortcut. If num is a power of two, we can just use a mask.
    ((= (logand num (1+ (lognot num))) num)
-    (logand (1- num) (random-u64)))
+    (logand (1- num) (random-uint bytes)))
    (#t
-    (let ((rand (random-u64)))
-      ;; The bias range consists of (mod max-u64 num) values at the end of the
-      ;; range. bias-start will be the first value that is biased.
-      (let ((bias-start (- max-u64 (mod max-u64 num))))
+    (let ((rand (random-uint bytes)))
+      ;; bias-start will be the first value that is biased.
+      (let ((bias-start (- (max-uint bytes)
+                           (bias-region (max-uint bytes) num))))
         ;; If this roll is in the bias range, then we need to resample to clear
         ;; that bias. Otherwise, we can mod the number and return it.
         (if (>= rand bias-start)
@@ -67,9 +94,10 @@
             (mod rand num)))))))
 
 ;; Generate a random phrase.
-(define (random-phrase)
+(define* (random-phrase
+          #:optional (bytes (optimal-random-bytes (vector-length phrases))))
   (vector-ref phrases
-              (uniform-random (vector-length phrases))))
+              (uniform-random (vector-length phrases) bytes)))
 
 ;; Logarithm base 2.
 (define (log2 val)
@@ -81,11 +109,12 @@
 ;; Generate a random password with the given minimum entropy (specified in
 ;; bits).
 (define (random-password min-entropy)
-  (string-concatenate
-   ;; Unfold a list of random phrases until we have achieved our minimum
-   ;; entropy, then concatenate them into a single string.
-   (unfold
-    (lambda (remn) (negative? remn))
-    (lambda (_) (random-phrase))
-    (lambda (remn) (- remn entropy-per-phrase))
-    min-entropy)))
+  (let ((bytes (optimal-random-bytes (vector-length phrases))))
+    (string-concatenate
+     ;; Unfold a list of random phrases until we have achieved our minimum
+     ;; entropy, then concatenate them into a single string.
+     (unfold
+      (lambda (remn) (negative? remn))
+      (lambda (_) (random-phrase bytes))
+      (lambda (remn) (- remn entropy-per-phrase))
+      min-entropy))))
